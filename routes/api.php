4e35b347c087823b674fffd7aabe766369ddd26e
zillur <zillurdeu@gmail.com>
2023-12-09 10:48:37 +0100

    feat: update user password

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::post('auth/login', Auth\LoginController::class);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
+    Route::put('password', Auth\PasswordUpdateController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
